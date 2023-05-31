3ebf246b58dbb5ed0501a75737c81a76b1c3c3f5
FAOUR Anas emmanuel <anasemmanuel.faour@infotel.com>
2023-05-31 14:21:36 +0200

    use ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Pokemon } from './pokemon';
 })
 export class AppComponent implements OnInit {
   pokemonList: Array<Pokemon> = POKEMONS;
+  selectedPokemon: Pokemon|undefined;
 
 
   ngOnInit(): void {
@@ -20,10 +21,22 @@ export class AppComponent implements OnInit {
     console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
   }
 
-  selectPokemonById(event : MouseEvent){
+  selectPokemonById(event: MouseEvent) {
     //const index : number = Number((event.target as HTMLInputElement).value); 
-    const index : number = +(event.target as HTMLInputElement).value; 
+    const index: number = +(event.target as HTMLInputElement).value;
     console.log(`Vous avez cliqué sur le pokémon ${this.pokemonList[index].name}`);
+  }
+
+  selectPokemonFromInput(pokemonId: string) {
+    const pokemon: Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId+1);
+    this.selectedPokemon = pokemon;
+   /* if (pokemon) {
+      console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
+      this.selectedPokemon = pokemon;
+    } else {
+      console.log(`Vous avez demandé un pokémon inexist`);
+      this.selectedPokemon = pokemon;
+    }*/
   }
 
 }
