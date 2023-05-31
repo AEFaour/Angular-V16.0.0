0e008d05bfbbecf3afd1f7da8a2049160308d36a
FAOUR Anas emmanuel <anasemmanuel.faour@infotel.com>
2023-05-31 16:12:25 +0200

    add & use Materialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Pokemon } from './pokemon';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent implements OnInit {
   pokemonList: Array<Pokemon> = POKEMONS;
