e331601d29d84f85b5405f3325d725d9ce151dae
FAOUR Anas emmanuel <anasemmanuel.faour@infotel.com>
2023-06-01 16:28:31 +0200

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemons-list';
-import { Pokemon } from './pokemon';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
-  pokemonList: Array<Pokemon> = POKEMONS;
-  selectedPokemon: Pokemon|undefined;
-
-
-  ngOnInit(): void {
-    console.table(this.pokemonList);
-    //this.pokemonList.map(pokemon => this.selectPokemon(pokemon));
-    //this.selectPokemon(this.pokemonList[3]);
-  }
-
-  selectPokemon(pokemon: Pokemon) {
-    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
-  }
-
-  selectPokemonById(event: MouseEvent) {
-    //const index : number = Number((event.target as HTMLInputElement).value); 
-    const index: number = +(event.target as HTMLInputElement).value;
-    console.log(`Vous avez cliqué sur le pokémon ${this.pokemonList[index].name}`);
-  }
-
-  selectPokemonFromInput(pokemonId: string) {
-    const pokemon: Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId+1);
-    this.selectedPokemon = pokemon;
-   /* if (pokemon) {
-      console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
-      this.selectedPokemon = pokemon;
-    } else {
-      console.log(`Vous avez demandé un pokémon inexist`);
-      this.selectedPokemon = pokemon;
-    }*/
-  }
-
+export class AppComponent {
 }
--- src/app/list-pokemon/list-pokemon.component.ts
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from '../mock-pokemons-list';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-list-pokemon',
   templateUrl: './list-pokemon.component.html',
-  styles: [
-  ]
 })
 export class ListPokemonComponent {
+  pokemonList: Array<Pokemon> = POKEMONS;
 
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+  }
+  
 }
