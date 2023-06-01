c7ee0051da7836513d8bf95f96a36ecf679d1bea
FAOUR Anas emmanuel <anasemmanuel.faour@infotel.com>
2023-06-01 15:32:13 +0200

    create list-pokemeon ans detail-pokemon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BorderCardDirective } from './border-card.directive';
 import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 
 
 @NgModule({
@@ -12,6 +14,8 @@ import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
     AppComponent,
     BorderCardDirective,
     PokemonTypeColorPipe,
+    ListPokemonComponent,
+    DetailPokemonComponent,
   ],
   imports: [
     BrowserModule,
