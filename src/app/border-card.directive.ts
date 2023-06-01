8fc701a2f170479baf96fc6320ace6c7524212b5
FAOUR Anas emmanuel <anasemmanuel.faour@infotel.com>
2023-06-01 11:52:37 +0200

    use directive for style with @Input()

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[pokemonBorderCard]'
@@ -10,14 +10,15 @@ export class BorderCardDirective {
     this.setBorder("#f5f5f5");
   }
 
+  @Input('pokemonBorderCard') borderColor: string;
+
   @HostListener('mouseenter') onMouseEnter() {
-    this.setBorder("violet");
+    this.setBorder(this.borderColor ||"violet");
   }
 
   @HostListener('mouseleave') onMouseLeave() {
     this.setBorder("#f5f5f5");
   }
-
 
   private setHeight(height: number) {
     this.el.nativeElement.style.height = `${height}rem`;
