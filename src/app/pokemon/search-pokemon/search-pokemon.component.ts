a255ee7b3644cf0f9431e7da6c3f9cf38ad2ac23
FAOUR Anas emmanuel <anasemmanuel.faour@infotel.com>
2023-06-07 16:23:40 +0200

    search pokemon

--- src/app/pokemon/search-pokemon/search-pokemon.component.ts
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PokemonService } from '../pokemon.service';
+import { Pokemon } from '../pokemon';
+import { Observable, Subject, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-search-pokemon',
   templateUrl: './search-pokemon.component.html',
-  styles: [
-  ]
 })
 export class SearchPokemonComponent {
+  searchTerms = new Subject<string>();
+  pokemons$: Observable<Array<Pokemon>>;
+
+  constructor(
+    private router: Router,
+    private pokemonService: PokemonService
+  ) { }
+
+  ngOnInit(): void {
+    this.pokemons$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.pokemonService.searchPokemonList(term))
+    );
+  }
+
+  search(term: string) {
+    this.searchTerms.next(term);
+  }
+
+  gotoDetail(pokemon: Pokemon) {
+    this.router.navigate(['/pokemon', pokemon.id]);
+  }
 
 }
